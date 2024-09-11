930c718fcb4bcd0304b8f7512187bc97edac1bd8
Fabrilio <bvsppress@gmail.com>
2024-09-11 20:24:11 +0700

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -1,25 +1,34 @@
-// enemy.js
-
-// Kelas Enemy merepresentasikan musuh yang harus dihindari atau ditembak oleh pemain
 class Enemy {
-    constructor(x, y, width, height, speed) {
-        this.x = x; // Posisi horizontal musuh
-        this.y = y; // Posisi vertikal musuh
-        this.width = width; // Lebar musuh
-        this.height = height; // Tinggi musuh
-        this.speed = speed; // Kecepatan musuh turun ke bawah layar
-        this.color = 'red'; // Warna musuh
-        
+    constructor(x, y, width, height, speed, points) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.color = randomColor();
+        this.points = points;
+        this.shape = Math.floor(Math.random() * 3);
     }
 
-    // Fungsi untuk menggerakkan musuh ke bawah
     move() {
-        this.y += this.speed; // Musuh bergerak ke bawah setiap frame
+        this.y += this.speed;
     }
 
-    // Fungsi untuk menggambar musuh di kanvas
     draw(ctx) {
-        ctx.fillStyle = this.color; // Mengatur warna musuh
-        ctx.fillRect(this.x, this.y, this.width, this.height, this.radius); // Menggambar musuh sebagai persegi panjang
+        ctx.fillStyle = this.color;
+        if (this.shape === 0) {
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        } else if (this.shape === 1) {
+            ctx.beginPath();
+            ctx.arc(this.x + this.width / 2, this.y + this.height / 2, this.width / 2, 0, Math.PI * 2);
+            ctx.fill();
+        } else if (this.shape === 2) {
+            ctx.beginPath();
+            ctx.moveTo(this.x + this.width / 2, this.y);
+            ctx.lineTo(this.x, this.y + this.height);
+            ctx.lineTo(this.x + this.width, this.y + this.height);
+            ctx.closePath();
+            ctx.fill();
+        }
     }
 }
