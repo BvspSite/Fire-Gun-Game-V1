b69e7a3491b7244107a53a5b84348bb782462be0
Fabrilio <bvsppress@gmail.com>
2024-09-11 20:22:46 +0700

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -1,24 +1,19 @@
-// bullet.js
-
-// Kelas Bullet merepresentasikan peluru yang ditembakkan oleh pemain
 class Bullet {
     constructor(x, y, width, height, speed) {
-        this.x = x; // Posisi horizontal peluru
-        this.y = y; // Posisi vertikal peluru
-        this.width = width; // Lebar peluru
-        this.height = height; // Tinggi peluru
-        this.speed = speed; // Kecepatan peluru naik ke atas layar
-        this.color = 'yellow'; // Warna peluru
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.color = 'yellow';
     }
 
-    // Fungsi untuk menggerakkan peluru ke atas
     move() {
-        this.y -= this.speed; // Peluru bergerak ke atas setiap frame
+        this.y -= this.speed;
     }
 
-    // Fungsi untuk menggambar peluru di kanvas
     draw(ctx) {
-        ctx.fillStyle = this.color; // Mengatur warna peluru
-        ctx.fillRect(this.x, this.y, this.width, this.height); // Menggambar peluru sebagai persegi panjang kecil
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
